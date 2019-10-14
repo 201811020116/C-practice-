92e047924922f4fd23ee44620d3b966d7128b5f2
201811020116 <44442543+201811020116@users.noreply.github.com>
2019-10-14 23:16:02 +0800

    Add files via upload

--- 2-5-3.cpp
+++ 2-5-3.cpp
@@ -5,14 +5,19 @@ int main()
     int *a =  new int[20];
     int  i,counter1 = 0, counter2 = 0;
     cout<<"Please enter a series of values, press 'n' when you have finished typing :  "<<endl;
-    while(i!=EOF)
+    while(1)
     {
-        if(cin >> i && i !=0)
+        cin >> i;
+        if( i !=0)
         {
             if(i>0)
                 counter1 ++;
             else
                 counter2 ++;
+        }
+        else
+        {
+            break;
         }
     }
     cout<<"正数个数是：\n"<<counter1<<endl;
